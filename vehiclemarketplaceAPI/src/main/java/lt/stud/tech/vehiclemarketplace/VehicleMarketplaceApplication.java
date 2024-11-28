9e966d073b51e332946014163ea0a97219bcca11
Darius Zilionis <Darius.Zilionis@aol.com>
2024-11-28 23:43:52 +0200

    initial commit

---
+++ vehiclemarketplaceAPI/src/main/java/lt/stud/tech/vehiclemarketplace/VehicleMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package lt.stud.tech.vehiclemarketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleMarketplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleMarketplaceApplication.class, args);
+	}
+
+}
---
+++ vehiclemarketplaceAPI/src/test/java/lt/stud/tech/vehiclemarketplace/VehicleMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.stud.tech.vehiclemarketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleMarketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
